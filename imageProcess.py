12f889a359becddfc1e31f552fd4c0c02875240d
Kgranv <60527591+Kgranv@users.noreply.github.com>
2024-11-11 10:33:33 +0100

    create script for dataset management
    - update script image processing
    - create script for generate/update dataset

--- imageProcess.py
+++ imageProcess.py
@@ -2,26 +2,24 @@ import os
 import json
 from PIL import Image
 
-def process_images(input_dir='rawImage', output_dir='processedImage', target_size=(64, 64), label='a'):
+def process_images(input_dir='./dataset/unprocessed/rawImage', output_dir='./dataset/processed', target_size=(64, 64), label='a'):
     """
     Génère une copie redimensionnée des images du dossier d'entrée dans un nouveau dossier au format PNG 
     et crée un fichier JSON contenant une liste des labels associés.
 
     Arguments :
     - input_dir (str) : Dossier contenant les images d'origine. Par défaut, 'rawImage'.
-    - output_dir (str) : Dossier de destination pour les images traitées. Par défaut, 'processedImage'.
+    - output_dir (str) : Dossier de destination parent pour les images traitées. Par défaut, 'processed'.
     - target_size (tuple) : Dimensions cibles des images (largeur, hauteur). Par défaut, (64, 64).
-    - label (str) : Label à associer à chaque image traitée, stocké dans le fichier JSON. Par défaut, 'a'.
+    - label (str) : Label à associer à chaque image traitée. Par défaut, 'a'.
 
     Résultat :
     - Les images sont redimensionnées à la taille spécifiée et enregistrées en PNG dans le dossier de destination.
     - Un fichier JSON contenant une liste de labels est créé dans le dossier de destination.
     """
     # Créer le dossier de destination s'il n'existe pas
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    labelList = []
+    if not os.path.exists(os.path.join(output_dir, label)):
+        os.makedirs(os.path.join(output_dir, label))
 
     # Parcourir toutes les images du dossier d'entrée
     for i, filename in enumerate(os.listdir(input_dir)):
@@ -36,19 +34,11 @@ def process_images(input_dir='rawImage', output_dir='processedImage', target_siz
 
                 # Format de nommage : 0000x.png
                 output_filename = f"{i:05d}.png"
-                output_path = os.path.join(output_dir, output_filename)
+                output_path = os.path.join(os.path.join(output_dir, label), output_filename)
 
                 img_resized.save(output_path, format='PNG')
 
-                labelList.append(label)
-
-    # Enregistrer la liste de statuts dans un fichier JSON
-    status_file_path = os.path.join(output_dir, "labels.json")
-    with open(status_file_path, 'w') as json_file:
-        json.dump(labelList, json_file)
-
-    print(f"Toutes les images ont été traitées et enregistrées dans le dossier '{output_dir}'.")
-    print(f"Le fichier de labels a été enregistré sous '{status_file_path}'.")
+    print(f"Toutes les images ont été traitées et enregistrées dans le dossier '{os.path.join(output_dir, label)}'.")
 
 # Exécution de la fonction
-process_images()
+process_images(label="e")
---
+++ updateDataset.py
@@ -0,0 +1,105 @@
+import os
+import shutil
+import numpy as np
+import h5py
+from PIL import Image
+
+def checkDatasetExist(datasetFileName):
+    if not os.path.exists(datasetFileName + ".h5"):
+        return False
+    return True
+
+def createDataset(images, labels, datasetFileName="dataset"):
+    if checkDatasetExist(datasetFileName):
+        print(f"Erreur : Le fichier {datasetFileName}.h5 existe déjà.")
+        return
+
+    with h5py.File(datasetFileName + ".h5", 'w') as f:
+        # Créer un dataset pour les images
+        f.create_dataset('images', data=images, maxshape=(None, images.shape[1], images.shape[2]), compression="gzip", chunks=True)
+        
+        # Créer un dataset pour les labels avec maxshape=(None,) pour permettre l'ajout de nouveaux labels
+        f.create_dataset('labels', data=labels, maxshape=(None,), dtype="S10", compression="gzip", chunks=True)
+
+        # Optionnel : Ajouter des métadonnées pour mieux organiser
+        f.attrs['description'] = 'Dataset d\'images pour classification'
+        f.attrs['source'] = 'Hiragana écrit a la main'
+
+    readDataset(datasetFileName)
+
+def appendDataset(newImages, newLabels, datasetFileName="dataset"):
+    if not checkDatasetExist(datasetFileName):
+        print(f"Erreur : Le fichier {datasetFileName}.h5 n'existe pas.")
+        return
+    
+    with h5py.File(datasetFileName + ".h5", 'a') as f:
+        # Récupérer les datasets existants
+        images = f['images']
+        labels = f['labels']
+
+        # Redimensionner les datasets pour ajouter les nouvelles données
+        images.resize((images.shape[0] + newImages.shape[0]), axis=0)
+        images[-newImages.shape[0]:] = newImages  # Ajouter les nouvelles images
+
+        labels.resize((labels.shape[0] + newLabels.shape[0]), axis=0)
+        labels[-newLabels.shape[0]:] = newLabels  # Ajouter les nouveaux labels
+
+    readDataset(datasetFileName)
+
+def readDataset(datasetFileName="dataset"):
+    with h5py.File(datasetFileName + ".h5", 'r') as f:
+        print("Contenu du fichier HDF5 :")
+        print(f.keys())
+
+        images = f['images'][:]
+        labels = f['labels'][:]
+
+        print("Images shape:", images.shape)
+        print("Labels:", labels)
+
+def load_images_from_folders(base_dir="./dataset/processed",archive_dir="./dataset/archive"):
+    images = []  
+    labels = []  
+
+    # Parcourir tous les sous-dossiers dans le dossier de base
+    for folder_name in os.listdir(base_dir):
+        folder_path = os.path.join(base_dir, folder_name)
+        
+        if os.path.isdir(folder_path):
+            # Parcourir tous les fichiers dans le dossier
+            for file_name in os.listdir(folder_path):
+                print(file_name)
+                file_path = os.path.join(folder_path, file_name)
+                
+                if file_name.lower().endswith(('.png', '.jpg', '.jpeg')):
+                    image = Image.open(file_path)
+                    # Convertir en niveau de gris
+                    image = image.convert("L")
+
+                    image_array = np.array(image)
+
+                    images.append(image_array)
+                    labels.append(folder_name)
+
+                    archive_folder = os.path.join(archive_dir, folder_name)
+                    if not os.path.exists(archive_folder):
+                        os.makedirs(archive_folder)
+
+                    # Déplacer l'image vers le dossier "archive"
+                    archived_image_path = os.path.join(archive_folder, file_name)
+                    shutil.move(file_path, archived_image_path)
+
+    images = np.array(images)
+    labels = np.array(labels, dtype="S10")
+
+    return images, labels
+
+
+images, labels = load_images_from_folders()
+if len(images)==0:
+    print("Rien à ajouter au dataset.")
+else:
+    #createDataset(images, labels)   # Crée le dataset initial
+    appendDataset(images, labels)   # Ajoute de nouvelles données
+
+#readDataset()     # Lis et affiche le contenu du dataset initial
