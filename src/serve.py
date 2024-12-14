69368baff7bb7e8e13b7b7c1eb7db98a4d821130
richard <richarda.tyarks@hes-so.ch>
2024-12-14 14:07:14 +0100

    test

--- src/serve.py
+++ src/serve.py
@@ -29,49 +29,27 @@ def upload_to_gcs(file_name, file_data, character):
     return f"File : {file_data}, uploaded to path : gs://{BUCKET_NAME}/{dest_path}"
 
 # bentoml serve --working-dir src
+
 @bentoml.service
 class HiraganaClassifierService:
 
-    # bento_model = bentoml.keras.get(MODEL_TITLE)
-    # # TODO : crash here
-    #
-    # def __init__(self) -> None:
-    #     self.preprocess = self.bento_model.custom_objects["preprocess"]
-    #     self.postprocess = self.bento_model.custom_objects["postprocess"]
-    #     self.model = self.bento_model.load_model()
-    #
-    # @bentoml.api()
-    # def predict(
-    #         self,
-    #         image: Annotated[PILImage, ContentType("image/png")] = Field(description="Hiragana image"),
-    #         letter_confirmation: str = Field(..., description="write which letter you drew"),
-    # ) -> Annotated[str, ContentType("application/json")]:
-    #     image = self.preprocess(image)
-    #     print(f"Hiragana drew : {letter_confirmation}")
-    #
-    #     predictions = self.model.predict(image)
-    #
-    #     return json.dumps(self.postprocess(predictions))
-
     @bentoml.api()
-    def upload_images(self, inputs: List[PILImage], label: str) -> List[str]:
+    def upload_images(self, image: Annotated[PILImage, ContentType("image/png")], label: str) -> str:
         """
-        Handle batch uploads of images.
+        Handle upload of image in google cloud.
         """
-        uploaded_urls = []
-        for i, image in enumerate(inputs, start=1):
+        uploaded_url = ""
 
-            # Generate a file name for the image
-            current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-            file_name = f"img_{current_time}_{label}.png"
+        # Generate a file name for the image
+        current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+        file_name = f"img_{current_time}_{label}.png"
 
-            # Save image to a buffer
-            with io.BytesIO() as buffer:
-                image.save(buffer, format="PNG")
-                buffer.seek(0)
+        # Save image to a buffer
+        with io.BytesIO() as buffer:
+            image.save(buffer, format="PNG")
+            buffer.seek(0)
 
-                # Upload image to GCS
-                uploaded_url = upload_to_gcs(file_name, buffer, label)
-                uploaded_urls.append(uploaded_url)
+            # Upload image to GCS
+            uploaded_url = upload_to_gcs(file_name, buffer, label)
 
-        return uploaded_urls
+        return uploaded_url
